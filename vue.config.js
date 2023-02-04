8460f74e8b1065eabdaf69e75608fc5f9885fe5e
Sergii Kochetov <sergii.koch@gmail.com>
2023-02-05 00:06:55 +0200

    Updated vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: "/reddit-clone/",
+  publicPath: "/vue2-reddit-clone/",
   transpileDependencies: true,
   pluginOptions: {
     i18n: {
