a0a2188258ce5de8c067ded0dafcb0c1c701ff34
Sergii Kochetov <sergii.koch@gmail.com>
2023-02-06 18:44:35 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ yarn build
 # git push -f git@github.com:watchlar2000/vue2-reddit-clone.git main:gh-pages
 
 git add dist -f
-git commit -m 'deploy'
+git commit -m "adding dist"
 git subtree push --prefix dist origin gh-pages
 
 cd -
